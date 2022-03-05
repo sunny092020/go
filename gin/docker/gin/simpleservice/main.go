22a2d593aa59f11da7b19f8f6b7deffd98705295
Vu Hoang Giang <itworkjp@gmail.com>
2022-03-05 17:06:21 +0700

    try router versioning

--- gin/docker/gin/simpleservice/main.go
+++ gin/docker/gin/simpleservice/main.go
@@ -2,17 +2,32 @@ package main
 import (
     "github.com/gin-gonic/gin"
 )
-func endpointHandler(c *gin.Context) {
-    c.String(200, "%s %s", c.Request.Method, c.Request.URL.Path)
+func v1EndpointHandler(c *gin.Context) {
+    c.String(200, "v1: %s %s", c.Request.Method, c.Request.URL.Path)
+}
+func v2EndpointHandler(c *gin.Context) {
+    c.String(200, "v2: %s %s", c.Request.Method, c.Request.URL.Path)
 }
 
 func main() {
     router := gin.Default()
-    router.GET("/products", endpointHandler)
-    router.GET("/products/:productId", endpointHandler)
-    // Eg: /products/1052
-    router.POST("/products", endpointHandler)
-    router.PUT("/products/:productId", endpointHandler)
-    router.DELETE("/products/:productId", endpointHandler)
+
+    v1 := router.Group("/v1")
+
+    v1.GET("/products", v1EndpointHandler)
+    // Eg: /v1/products
+    v1.GET("/products/:productId", v1EndpointHandler)
+    v1.POST("/products", v1EndpointHandler)
+    v1.PUT("/products/:productId", v1EndpointHandler) 
+    v1.DELETE("/products/:productId", v1EndpointHandler)
+
+    v2 := router.Group("/v2")
+
+    v2.GET("/products", v2EndpointHandler)
+    v2.GET("/products/:productId", v2EndpointHandler)
+    v2.POST("/products", v2EndpointHandler)
+    v2.PUT("/products/:productId", v2EndpointHandler)
+    v2.DELETE("/products/:productId", v2EndpointHandler)
+
     router.Run(":5000")
 }
