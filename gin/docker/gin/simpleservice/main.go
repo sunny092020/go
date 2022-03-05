b9251a267de087f92992e981f3c0ffa26be59107
Vu Hoang Giang <itworkjp@gmail.com>
2022-03-05 17:12:15 +0700

    create add number service

--- gin/docker/gin/simpleservice/main.go
+++ gin/docker/gin/simpleservice/main.go
@@ -1,5 +1,7 @@
 package main
 import (
+    "fmt"
+    "strconv"
     "github.com/gin-gonic/gin"
 )
 func v1EndpointHandler(c *gin.Context) {
@@ -9,8 +11,15 @@ func v2EndpointHandler(c *gin.Context) {
     c.String(200, "v2: %s %s", c.Request.Method, c.Request.URL.Path)
 }
 
+func add(c *gin.Context) {
+    x, _ := strconv.ParseFloat(c.Param("x"), 64)
+    y, _ := strconv.ParseFloat(c.Param("y"), 64)
+    c.String(200,  fmt.Sprintf("%f", x + y))
+}
+
 func main() {
     router := gin.Default()
+    router.GET("/add/:x/:y", add)
 
     v1 := router.Group("/v1")
 
