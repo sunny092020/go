62e278bb4574a1d6874c0e9b49d7144919a9f4a3
Vu Hoang Giang <itworkjp@gmail.com>
2022-03-05 17:23:44 +0700

    Validating incoming requests

--- gin/docker/gin/simpleservice/main.go
+++ gin/docker/gin/simpleservice/main.go
@@ -25,6 +25,11 @@ type AddParams struct {
     Y float64 `json:"y"`
 }
 
+type PrintJob struct {
+    JobId int `json:"jobId" binding:"required,gte=10000"`
+    Pages int `json:"pages" binding:"required,gte=1,lte=100"`
+}
+
 func addPost(c *gin.Context) {
     var ap AddParams
     if err := c.ShouldBindJSON(&ap); err != nil {
@@ -37,6 +42,17 @@ func addPost(c *gin.Context) {
 
 func main() {
     router := gin.Default()
+    
+    router.POST("/print", func(c *gin.Context) {
+        var p PrintJob
+        if err := c.ShouldBindJSON(&p); err != nil {
+            c.JSON(400, gin.H{"error": "Invalid input!"})
+            return
+        }
+        c.JSON(200, gin.H{"message": 
+            fmt.Sprintf("PrintJob #%v started!", p.JobId)})
+    })
+
     router.GET("/add/:x/:y", add)
     router.POST("/add", addPost)
     v1 := router.Group("/v1")
